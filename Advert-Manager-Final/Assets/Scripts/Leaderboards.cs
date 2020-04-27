05877bd48a43e7fa67c042da13aa8cd5cc61737b
DarrenGodley <32136209+DarrenGodley@users.noreply.github.com>
2020-04-27 18:02:44 +0100

    Leaderboard/Achievement

---
+++ Advert-Manager-Final/Assets/Scripts/Leaderboards.cs
@@ -0,0 +1,26 @@
+﻿using GooglePlayGames.BasicApi.Multiplayer;
+using UnityEngine;
+
+public class Leaderboards : MonoBehaviour
+{
+    public void OpenLeaderboard()
+    {
+        Social.ShowLeaderboardUI();
+    }
+
+    public void UpdateLeaderboardScore()
+    {
+        if (PlayerPrefs.GetInt("ScoreToUpdate", 0) == 0)
+        {
+            return;
+        }
+
+        Social.ReportScore(PlayerPrefs.GetInt("ScoreToUpdate", 1), GPGSIds.leaderboard_high_score, (bool success) =>
+        {
+            if (success)
+            {
+                PlayerPrefs.SetInt("ScoreToUpdate", 0);
+            }
+        });
+    }
+}
--- Advert-Manager-Final/Assets/Scripts/Score.cs
+++ Advert-Manager-Final/Assets/Scripts/Score.cs
@@ -1,8 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     public static int score = 0;
+    public Text scoreText;
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+
+        PlayerPrefs.SetInt("ScoreToUpdate", PlayerPrefs.GetInt("ScoreToUpdate", 0) + 1);
+    }
 }
